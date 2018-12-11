9ecffb59af48ae77ba5565afbab25c80cd32509d
Amos Xia <xiayy860612@126.com>
2018-12-12 07:30:38 +0800

    issue@1 impl oauth

---
+++ src/main/java/com/s2u2m/iam/IamApplication.java
@@ -0,0 +1,18 @@
+package com.s2u2m.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * IamApplication create on 2018/12/11
+ *
+ * @author Amos Xia
+ */
+@SpringBootApplication
+public class IamApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamApplication.class, args);
+    }
+}
