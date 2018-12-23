0e616cf8a2c2704fecd0c431c19a4f62cc11cd82
Amos Xia <xiayy860612@126.com>
2018-12-23 22:34:44 +0800

    issue@1 set up oauth2 in memory

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # identity-access-management
+
+## 参考资料
+
+- [理解OAuth 2.0](http://www.ruanyifeng.com/blog/2014/05/oauth_2_0.html)
+- [从零开始的Spring Security Oauth2（一）](http://blog.didispace.com/spring-security-oauth2-xjf-1/)
---
+++ src/main/java/com/s2u2m/iam/config/security/ResourceSecurityConfig.java
@@ -0,0 +1,48 @@
+/*
+ *    Copyright [2018] [s2u2m]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.s2u2m.iam.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/**
+ * ResourceSecurityConfig create on 2018/12/23
+ *
+ * @author Amos Xia
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceSecurityConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .requestMatchers()
+                .antMatchers("/api/**");
+    }
+}
