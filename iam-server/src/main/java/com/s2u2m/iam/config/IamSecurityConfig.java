f6b9ace1fe77326082e45ee2322e177ddddc2174
Amos Xia <xiayy860612@126.com>
2019-02-12 22:00:01 +0800

    issue#1 set up server project

---
+++ iam-server/src/main/java/com/s2u2m/iam/config/IamSecurityConfig.java
@@ -0,0 +1,52 @@
+/*
+ *    Copyright [2019] [s2u2m]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.s2u2m.iam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * IamSecurityConfig create on 19-2-12.
+ *
+ * @author Amos Xia
+ */
+@Configuration
+@EnableWebSecurity
+public class IamSecurityConfig {
+
+    @Configuration
+    public static class UsernameSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("*/swagger*", "/v2/api-docs").permitAll();
+//                    .anyRequest().authenticated()
+//                    .and()
+//                    .httpBasic();
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            // config AuthenticationProvider into ProviderManager
+        }
+    }
+}
